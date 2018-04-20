4005d50ea7482766b9911924088cabed643bf5fb
cigar9 <cigar9.web@gmail.com>
2018-04-20 12:37:04 +0900

    アコーディオンの処理を変更

--- src/js/main.js
+++ src/js/main.js
@@ -11,30 +11,21 @@
 // import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap'
 
 // jQuery easing
-import 'jquery.easing'
+// import 'jquery.easing'
 
 //////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 // ページトップへ戻る
-import pagetop from './modules/pagetop'
-pagetop()
+// import pagetop from './modules/pagetop'
+// pagetop()
 
-import spNavi from './modules/sp-navi'
-spNavi()
+// import spNavi from './modules/sp-navi'
+// spNavi()
 
-import accordion from './modules/accordion-collapse'
-accordion()
+// import accordion from './modules/accordion-collapse'
+// accordion()
 
-import faqAccordion from './modules/faq-accordion'
-faqAccordion()
-
-import formSubjectChange from './modules/form-subject-change'
-formSubjectChange()
-
-import floatingBanner from './modules/floating-banner'
-floatingBanner()
-
-import gnaviActive from './modules/gnavi-active'
-gnaviActive()
+// import gnaviActive from './modules/gnavi-active'
+// gnaviActive()
--- src/js/modules/accordion-collapse.js
+++ src/js/modules/accordion-collapse.js
@@ -1,30 +1,78 @@
-export default () => {
-  const $btn = $('.js-accordion-btn-sm')
-  const $body = $('.js-accordion-body-sm')
+// レスポンシブ対応（幅をグリグリ変えても大丈夫）のアコーディオン
+// .js-accordion-btnのdata-accordionに対応したidを持つDOMが開閉ターゲットとなる。
+// .js-accordion-btn, .js-accordion-body に'data-mq'を設定し、アコーディオン化するウィンドウ幅を決定する。
+// lg = 全ての幅でアコーディオン化
+// md = 960px以下でアコーディオン化
+// sm = 600px以下でアコーディオン化
+//
+// ex.
+// <p class="js-accordion-btn" data-accordion="#hogehoge" data-mq="lg">hogehogeアコーディオンを開く</p>
+// <div id="hogehoge" class="js-accordion-body" data-mq="lg">
+//   hogehogeアコーディオンのコンテンツ
+// </div>
+//
+// ※「PC幅ではアコーディオンだけど、SP幅ではアコーディオンやめる」というのはできない。
 
-  window.matchMedia('(max-width: 600px)').addListener(toggleAccordionBody)
-  function toggleAccordionBody() {
-    if (window.innerWidth <= 600) {
-      $body.hide()
-      // $btn.removeClass('is-active')
-      $body.each(function() {
-        if ($(this).hasClass('is-open')) {
-          $(this).show()
-        }
-      })
-      $btn.on('click', function() {
+export default () => {
+  const $btn = $('.js-accordion-btn'),
+    $body = $('.js-accordion-body')
+
+  // is-openがついていたら初期表示でオープン
+  $body.each(function() {
+    if ($(this).hasClass('is-open')) {
+      $(this).show()
+    }
+  })
+
+  // クリック時の処理
+  $btn.on('click', function() {
+    const $body = $($(this).attr('data-accordion'))
+
+    const mq = $(this).attr('data-mq')
+    if (mq == 'sm') {
+      // console.log('sm')
+
+      if (window.innerWidth <= 600) {
         const $body = $($(this).attr('data-accordion'))
-        if ($body.is(':animated')) {
-          return false
-        } else {
-          $(this).toggleClass('is-active')
-          $body.stop().slideToggle(800, 'easeOutExpo')
-        }
-      })
+        toggleAccordion($(this), $body)
+      }
+    } else if (mq === 'md') {
+      // console.log('md')
+      if (window.innerWidth <= 960) {
+        const $body = $($(this).attr('data-accordion'))
+        toggleAccordion($(this), $body)
+      }
     } else {
-      $body.show()
-      $btn.off('click')
+      // console.log('lg')
+      const $body = $($(this).attr('data-accordion'))
+      toggleAccordion($(this), $body)
+    }
+  })
+
+  function toggleAccordion(btn, body) {
+    if (body.is(':animated')) {
+      return false
+    } else {
+      btn.toggleClass('is-active')
+      body.stop().slideToggle(800, 'easeOutExpo')
     }
   }
-  toggleAccordionBody()
+
+  // resize時の処理
+  window.matchMedia('(max-width: 600px)').addListener(mqSm)
+  window.matchMedia('(max-width: 960px)').addListener(mqMd)
+  function mqSm() {
+    const $bodySm = $('.js-accordion-body[data-mq="sm"]')
+    if (window.innerWidth > 600) {
+      // console.log('size is sm')
+      $bodySm.show()
+    }
+  }
+  function mqMd() {
+    const $bodyMd = $('.js-accordion-body[data-mq="md"]')
+    if (window.innerWidth > 960) {
+      // console.log('size is md')
+      $bodyMd.show()
+    }
+  }
 }
